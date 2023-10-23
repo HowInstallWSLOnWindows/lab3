ca25a68c118cace61678acc61e3a7a9895bf9031
HowInstallWSLOnWindows <92132038+HowInstallWSLOnWindows@users.noreply.github.com>
2023-10-23 15:49:26 -0700

    made testFiltert to test filter method
    have some of the neccesssary stuff down just need the StringChecker part to be done

--- ListTests.java
+++ ListTests.java
@@ -1,6 +1,14 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ListTests {
-    
+    @Test
+    public void testFilter(){
+        List<String> pump = new ArrayList<>();
+        Collections.addAll(pump,"mokney", "donkey", "funk", "bum", "homeless");
+        assertArrayEquals("monkey", ListExamples(pump, "monkey"));
+    }
 }
